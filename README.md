a28545f86144dd469185ac218bf79d0e9c5f4c53
lukexyz <lukedwoods@gmail.com>
2021-10-24 04:21:18 +0100

    first attempt of the unicorn company

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # thethefacebook
+A possible unicorn company, based on the great idea by Mark Zuckerberg.
+
+# Acknowledgements
+All credit to Mark Zuckerburg.
---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from PIL import Image
+
+st.image(Image.open('media/facemash_header.JPG'))
+
+ 
+p1, p2, p3 = st.columns((6,1,6))
+
+p1.image(Image.open('media/zuckerburg_white.png'))
+p1.button('Choice 1')
+
+p2.write("or")
+p3.image(Image.open('media/agatha-vega-again.jpg'))
+
+p3.button('Choice 2')
+
+st.write("All credit to Mark Zuckerberg")
